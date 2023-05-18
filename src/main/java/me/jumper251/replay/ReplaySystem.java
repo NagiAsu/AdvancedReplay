2f91aca41d2e10503a59ec53c80581db407c6ca8
MiunaPA <fish126732@gmail.com>
2023-05-18 15:39:48 +0800

    feat : 新增自動記錄系統

--- src/main/java/me/jumper251/replay/ReplaySystem.java
+++ src/main/java/me/jumper251/replay/ReplaySystem.java
@@ -1,13 +1,16 @@
 package me.jumper251.replay;
 
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
-
-
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-
-
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitScheduler;
+import me.jumper251.replay.api.ReplayAPI;
 import me.jumper251.replay.filesystem.ConfigManager;
 import me.jumper251.replay.filesystem.saving.DatabaseReplaySaver;
 import me.jumper251.replay.filesystem.saving.DefaultReplaySaver;
@@ -61,12 +64,33 @@ public class ReplaySystem extends JavaPlugin {
 			ReplayCleanup.cleanupReplays();
 		}
 		
-		getLogger().info("Finished (" + (System.currentTimeMillis() - start) + "ms)");
-
+        getLogger().info("Finished (" + (System.currentTimeMillis() - start) + "ms)");
+        if (ConfigManager.RECORD_STARTUP) {
+            getLogger().info("Auto Record Time : " + ConfigManager.MAX_LENGTH + " sec");
+            autoRecordTask();
+        }
 	}
 	
 	
 	public static ReplaySystem getInstance() {
 		return instance;
 	}
+
+    void autoRecordTask() {
+        BukkitScheduler scheduler = getServer().getScheduler();
+        scheduler.scheduleSyncRepeatingTask(this, () -> {
+            if (Bukkit.getOnlinePlayers().size() > 0) {
+                String formattedDateTime =
+                        LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyMMdd_HHmmss"));
+                ReplayAPI.getInstance().recordReplay(formattedDateTime,
+                        new ArrayList<Player>(Bukkit.getOnlinePlayers()));
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        ReplayAPI.getInstance().stopReplay(formattedDateTime, true, true);
+                    }
+                }.runTaskLater(ReplaySystem.getInstance(), 20 * ConfigManager.MAX_LENGTH);
+            }
+        }, 0L, 20 * ConfigManager.MAX_LENGTH);
+    }
 }
